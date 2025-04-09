675f4bac5cf2f176732f70139a84e95004b05fe4
sahil <sahil27raj@gmail.com>
2025-04-10 01:18:25 +0530

    first commit

---
+++ src/main/java/com/edigest/myFirstProject/MyClass.java
@@ -0,0 +1,14 @@
+package com.edigest.myFirstProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyClass {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+
+}
